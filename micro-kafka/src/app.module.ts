fc1d0e2635d89cbebd698b0caf10b31393342a1f
Jipson Medardo Saad Intriago <jsaad@Todo1of.com.ec>
2022-04-29 16:20:47 -0500

    Add kafkajs to the microservice

--- micro-kafka/src/app.module.ts
+++ micro-kafka/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { DefaultModule } from './app/default/default.module';
+import { ConsumerModule } from './app/consumer/consumer.module';
+import { KafkaModule } from './app/common/kafka.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    KafkaModule.register({
+      clientId: 'test-app-client',
+      brokers: ['localhost:9092'],
+      groupId: 'test-app-group',
+    }),
+    DefaultModule,
+    ConsumerModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
